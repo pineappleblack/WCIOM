e1cfef4c63243335eb24084017e197c459f362a9
Alexey Smagin <asblackpn@gmail.com>
2018-02-14 13:09:58 +0300

    start

---
+++ GenerateJSON.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[8]:
+
+
+def getJSON():
+    nicknames = {}
+    with open('nicknames.csv', encoding='utf-8') as f:
+        columns = f.readline().rstrip().split(';');
+        for line in f:
+            info = line.rstrip().split(';');
+            el = {}
+            for col, inf in zip(columns[1:], info[1:]):
+                el[col] = inf
+            nicknames[info[0]] = el
+    
+    mounthInfo = {}
+    with open('mounthInfo.csv', encoding='utf-8') as f:
+        for line in f:
+            el = line.rstrip().split(';')
+            mounthInfo[el[0]] = el[1]
+            
+    url = 'https://wciom.ru/news/ratings/vybory_2018/'
+    r = requests.get(url, verify = False)
+    soup = BeautifulSoup(r.text, 'lxml')
+    table = soup.findAll('table', {'id': 'tableprez'})[-1]
+    date = table.tr('th')[-1].text
+    tmpDate = date.replace(',', '').split()
+    date = tmpDate[1]+'.'+mounthInfo[tmpDate[0].lower()]+'.'+tmpDate[2]
+    info = [(row.findAll('td')[0].text, row.findAll('td')[-1].text)  for row in table.findAll('tr')[1:]]
+    
+    result = {}
+    result[date] = {'candidates': [], 'others': []}
+    for el in info:
+        candidate = {}
+        candidate['name'] = nicknames[el[0]]['name']
+        candidate['title'] = nicknames[el[0]]['title']
+        candidate['value'] = el[1]
+        if candidate['name'] not in "other noanswer notcome spoiler":
+            result[date]['candidates'].append(candidate)
+        else:
+            result[date]['others'].append(candidate)
+        
+        with open('wciom_candidates_2018.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(result))
+
+
+# In[9]:
+
+
+getJSON()
+
---
+++ bot.py
@@ -0,0 +1,48 @@
+﻿from GenerateJSON import getJSON
+import telebot
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import logging
+import time
+
+token = '436416709:AAH7sPnu-TeDUxF-JDlMm9OGrA5274AoIgo'
+
+bot = telebot.TeleBot(token)
+
+def send_message_to_channel():
+    with open('testfile', 'w', encoding = 'UTF-8') as f:
+        f.write(datetime.now().strftime("%A, %d. %B %Y %I:%M:%S") + '\n')
+    
+    with open('lastDate', encoding = 'UTF-8') as f:
+        lastDate = f.read()
+    
+    url = 'https://wciom.ru/news/ratings/vybory_2018/'
+    r = requests.get(url, verify = False)
+    soup = BeautifulSoup(r.text, 'lxml')
+    table = soup.findAll('table', {'id': 'tableprez'})[-1]
+    date = table.tr('th')[-1].text
+    
+    if lastDate != date:
+        bot.send_message('@wciomrbc', 'Обновление от '+date)
+        getJSON()
+        with open('lastDate', 'w', encoding = 'UTF-8') as f:
+            f.write(date)
+        f = open('wciom_candidates_2018.json')
+        bot.send_document('@wciomrbc', f)
+        f.close()
+    time.sleep(1)
+    return
+	
+if __name__ == '__main__':
+    # Избавляемся от спама в логах от библиотеки requests
+    logging.getLogger('requests').setLevel(logging.CRITICAL)
+    # Настраиваем наш логгер
+    logging.basicConfig(format='[%(asctime)s] %(filename)s:%(lineno)d %(levelname)s - %(message)s', level=logging.INFO,
+                        filename='bot_log.log', datefmt='%d.%m.%Y %H:%M:%S')
+    while True:
+        send_message_to_channel()
+        logging.info('[App] Script went to sleep.')
+        time.sleep(5*60)
+
+    logging.info('[App] Script exited.\n')
